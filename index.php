887f7c6b61016a9cb79a08275a5def5e8acee231
orlovkn <orlovkn.job@gmail.com>
2020-12-21 06:58:45 +0300

    add strategy factory

--- index.php
+++ index.php
@@ -4,3 +4,16 @@ function __autoload($class_name)
 {
     include 'classes/' . $class_name . '.php';
 }
+
+$amount = 600;
+
+/*if ($amount >= 500) {
+    $pay = new PayByCC();
+    $pay->pay($amount);
+} else {
+    $pay = new PayByPayPal();
+    $pay->pay($amount);
+}*/
+
+$cart = new ShoppingCart($amount);
+$cart->payAmount();
