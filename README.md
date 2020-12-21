7febd9c3c5cf55b9ca0dfc57363b1fe9ba9af3a0
orlovkn <orlovkn.job@gmail.com>
2020-12-21 17:56:05 +0300

    add singleton

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# patterns.php
+# The Singleton Pattern
--- index.php
+++ index.php
@@ -4,3 +4,7 @@ function __autoload($class_name)
 {
     include 'classes/' . $class_name . '.php';
 }
+
+$object = Connection::getInstance();
+$object1 = Connection::getInstance();
+$object2 = Connection::getInstance();
